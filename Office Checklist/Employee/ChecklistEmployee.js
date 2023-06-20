1620d86a4501936341588f9759853caab7dc3f8e
kevkimbu <kevkim@bu.edu>
2023-06-20 13:57:48 -0400

    Employee sheet gets correct data values

--- Office Checklist/Employee/ChecklistEmployee.js
+++ Office Checklist/Employee/ChecklistEmployee.js
@@ -27,29 +27,30 @@ function onOpen() {
       var employeeName = employeeNames[i];
       var employeeSheet = employeeSpreadsheet.insertSheet(employeeName);
   
-      // Get task names from the admin sheet
-      var taskNames = adminSheet.getRange(3, 3, 1, adminSheet.getLastColumn() - 2).getValues()[0];
-  
-      // Set headers in employee sheet
-      var headers = [taskNames];
-      employeeSheet.getRange(1, 1, 1, headers[0].length).setValues(headers);
-  
       // Filter tasks for the current employee
-      var tasks = adminSheet.getRange(3, 3, adminSheet.getLastRow() - 2, adminSheet.getLastColumn() - 2)
+      var filteredTasks = adminSheet.getRange(3, 1, adminSheet.getLastRow() - 2, adminSheet.getLastColumn())
         .getValues()
         .filter(function (row) {
-          return row[0] === employeeName && row.slice(2).some(function (task) { return task !== ''; }); // Exclude rows without any task
+          return row[1] === employeeName && row.slice(2).some(function (task) { return task !== ''; }); // Exclude rows without any task
         });
   
+      // Get task names from the filtered tasks
+      var taskNames = filteredTasks.length > 0 ? filteredTasks[0].slice(2) : [];
+  
+      // Set headers in employee sheet
+      var headers = [''].concat(taskNames);
+      employeeSheet.getRange(1, 1, 1, headers.length).setValues([headers]);
+  
       // Copy filtered tasks to employee sheet
-      if (tasks.length > 0) {
-        employeeSheet.getRange(2, 1, tasks.length, 1).setValues(tasks.map(function (row) {
-          return [''];
-        }));
+      if (filteredTasks.length > 0) {
+        var taskData = filteredTasks.map(function (row) {
+          return [''].concat(row.slice(2));
+        });
+        employeeSheet.getRange(2, 1, taskData.length, taskData[0].length).setValues(taskData);
       }
   
       // Apply formatting to employee sheet
-      var range = employeeSheet.getRange('A1:A' + (tasks.length + 1));
+      var range = employeeSheet.getRange(1, 1, filteredTasks.length + 1, taskNames.length + 1);
       range.setHorizontalAlignment('center');
       range.setVerticalAlignment('middle');
     }
